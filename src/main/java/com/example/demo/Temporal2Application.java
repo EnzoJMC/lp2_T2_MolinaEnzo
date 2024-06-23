d01f02c96e813101b6919f158ff67361495e7df9
EnzoJMC <enzomolinacajas@gmail.com>
2024-06-22 19:13:50 -0500

    inicio del proyecto

---
+++ src/main/java/com/example/demo/Temporal2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Temporal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Temporal2Application.class, args);
+	}
+
+}
